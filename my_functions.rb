44832e1d17588ad39ae8935c56f94b7f64bd9605
L3n1ad <danieltoth0910@gmail.com>
2019-10-16 16:09:06 +0100

    create find item in an array function

--- my_functions.rb
+++ my_functions.rb
@@ -1,8 +1,11 @@
 
+# ADD the length of two string together
+
 def add_array_lengths(array_1, array_2)
   return array_1.length + array_2.length
 end
 
+# function return the sum of the array values
 
 def sum_array(num)
   sum = 0
@@ -10,4 +13,16 @@ def sum_array(num)
     sum = sum + int
   end
   return sum
+end
+
+# find an element in an array then return true otherwise return false
+# arr.each{ |a| return true if a == item}
+
+def is_item_in_array(array, item)
+  for search in array
+    if search == item
+      return true
+    end
+  end
+  return false
 end
