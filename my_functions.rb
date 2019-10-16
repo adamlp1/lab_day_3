1da21d680ef8ea9c12bd8863cb00a0cbcb2003e1
L3n1ad <danieltoth0910@gmail.com>
2019-10-16 15:55:16 +0100

    test second function

--- my_functions.rb
+++ my_functions.rb
@@ -2,3 +2,12 @@
 def add_array_lengths(array_1, array_2)
   return array_1.length + array_2.length
 end
+
+
+def sum_array(num)
+  sum = 0
+  for int in num
+    sum = sum + int
+  end
+  return sum
+end
